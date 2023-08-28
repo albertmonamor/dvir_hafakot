2f62b830574e247b4b59bc00068ed7c8802c50fa
saban <sabann248@gmail.com>
2023-08-28 19:33:31 +0300

    1.0.0 - starting for ads, phone, desktop

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,6 +1,4 @@
 
-
-
 function startTalk(_type){
     if (_type == 0){
         window.open("https://wa.me/+9720546761266?text=היי!, אשמח להשכיר ציוד לתאריך","_blank")
@@ -8,5 +6,9 @@ function startTalk(_type){
     else if (_type == 1){
         location.href = "tel:0529171171";
     }
-    
+    else{
+        /* somthing broken */
+        location.href = "tel:0546761266"
+    }
+
 }
